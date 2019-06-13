e6a09126aed02d5b852f0cb789d364ea33bfcdda
Annalie Bruns <eilanna7531@gmail.com>
2019-06-13 20:33:30 +0100

    DressUp Game Start. Click and Hat shall apear

---
+++ Unity/DressUp Game/Assets/Script/HatSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HatSpawner : MonoBehaviour
+{
+    private int rand;
+    public Sprite[] Sprite_Pic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<SpriteRenderer>().sprite = Sprite_Pic[rand];
+        }
+    }
+
+
+}
